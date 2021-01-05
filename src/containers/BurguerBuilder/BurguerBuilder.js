da22d9141c895b686a23ae8cd7af37d06be08a4d
Lucas Neves <lucas.neves@codeblox.com.br>
2021-01-05 18:52:28 -0300

    Loader

--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import Aux from '../../hoc/Aux/Aux';
-import Burguer from '../../components/Burguer/Burguer';
-import BuildControls from '../../components/Burguer/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burguer/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux/Aux";
+import Burguer from "../../components/Burguer/Burguer";
+import BuildControls from "../../components/Burguer/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burguer/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 const INGREDIENT_PRICES = {
   salad: 0.4,
   cheese: 0.3,
   meat: 0.4,
-  bacon: 1
-}
+  bacon: 1,
+};
 
 class BurguerBuilder extends Component {
   state = {
@@ -19,30 +20,32 @@ class BurguerBuilder extends Component {
       salad: 0,
       bacon: 0,
       cheese: 0,
-      meat: 0
+      meat: 0,
     },
     totalPrice: 4,
     purchasable: false,
-    purchasing: false
-  }
+    purchasing: false,
+    loading: false,
+  };
 
   updatePurchaseState(ingredients) {
-
-    const sum = Object.keys(ingredients).map(igKey => {
-      return ingredients[igKey]
-    }).reduce((sum, el) => {
-      return sum + el
-    }, 0)
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
     this.setState({
-      purchasable: sum > 0
-    })
+      purchasable: sum > 0,
+    });
   }
 
   addIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type]
+    const oldCount = this.state.ingredients[type];
     const updatedCount = oldCount + 1;
     const updatedIngredients = {
-      ...this.state.ingredients
+      ...this.state.ingredients,
     };
     updatedIngredients[type] = updatedCount;
     const priceAddition = INGREDIENT_PRICES[type];
@@ -50,19 +53,19 @@ class BurguerBuilder extends Component {
     const newPrice = oldPrice + priceAddition;
     this.setState({
       totalPrice: newPrice,
-      ingredients: updatedIngredients
-    })
+      ingredients: updatedIngredients,
+    });
     this.updatePurchaseState(updatedIngredients);
-  }
+  };
 
   removeIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type]
+    const oldCount = this.state.ingredients[type];
     if (oldCount <= 0) {
       return;
     }
     const updatedCount = oldCount - 1;
     const updatedIngredients = {
-      ...this.state.ingredients
+      ...this.state.ingredients,
     };
     updatedIngredients[type] = updatedCount;
     const priceDeduction = INGREDIENT_PRICES[type];
@@ -70,25 +73,26 @@ class BurguerBuilder extends Component {
     const newPrice = oldPrice - priceDeduction;
     this.setState({
       totalPrice: newPrice,
-      ingredients: updatedIngredients
-    })
+      ingredients: updatedIngredients,
+    });
     this.updatePurchaseState(updatedIngredients);
-  }
+  };
 
   purchasaHandler = () => {
-    this.setState({
-      purchasing: true
-    })
-  }
+    this.setState({ purchasing: true });
+  };
 
   purchaseCancelHandler = () => {
     this.setState({
       purchasing: false
-    })
-  }
+    });
+  };
 
   purchaseContinueHandler = () => {
     // alert('you continue')
+    this.setState({
+      loading: true
+    });
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
@@ -97,33 +101,53 @@ class BurguerBuilder extends Component {
         address: {
           street: "Rua Retiro",
           zipCode: "000000000",
-          country: "Brasil"
+          country: "Brasil",
         },
-        email: 'lneves@gmail.com'
+        email: "lneves@gmail.com",
       },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
-  }
-
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then(() => {
+        console.log(this.state.loading)
+      })
+      .catch((error) => {
+        
+      })
+      .finally(() => {
+      });
+  };
 
   render() {
     const disabledInfo = {
-      ...this.state.ingredients
-    }
+      ...this.state.ingredients,
+    };
     for (let key in disabledInfo) {
-      disabledInfo[key] = disabledInfo[key] <= 0
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
+    let orderSummary = (
+      <OrderSummary
+        totalPrice={this.state.totalPrice}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinue={this.purchaseContinueHandler}
+        ingredients={this.state.ingredients}
+      />
+    );
+
+    if (this.state.loading) {
+      orderSummary = (
+        <Spinner />
+      );
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary
-            totalPrice={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinue={this.purchaseContinueHandler}
-            ingredients={this.state.ingredients} />
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          {orderSummary}
         </Modal>
         <Burguer ingredients={this.state.ingredients} />
         <BuildControls
@@ -132,9 +156,10 @@ class BurguerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           purchasable={this.state.purchasable}
-          price={this.state.totalPrice} />
+          price={this.state.totalPrice}
+        />
       </Aux>
-    )
+    );
   }
 }
 
