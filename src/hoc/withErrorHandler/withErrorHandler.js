9e2fd967748aeb02bedd593f52433746a37a1c5e
Lucas Neves <lucas.neves@codeblox.com.br>
2021-01-06 18:37:47 -0300

    with error

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -5,20 +5,22 @@ import Modal from "../../components/UI/Modal/Modal";
 import Aux from "../Aux/Aux";
 
 const withErrorHandler = (WrappedComponent, axios) => {
-  
   return class extends Component {
-    state =  {
-      error: null
-    }
-    componentWillMount() {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+      };
       axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(res => res, (error) => {
-        this.setState({ error: error });
-
-      });
+      axios.interceptors.response.use(
+        (res) => res,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
     }
 
     errorConfirmed = () => {
